87b1a87db7b93376d8d5d05745c21f86344fa167
Joseph Chen <36026211+TotesOates@users.noreply.github.com>
2018-07-11 12:12:35 -0500

    Add files via upload

---
+++ RestaurantScrape.py
@@ -0,0 +1,19 @@
+import requests
+from bs4 import BeautifulSoup
+from selenium import webdriver
+
+# driver = webdriver.Chrome('C:\\Users\\Joe\\Desktop\\chromedriver_win32\\chromedriver')
+# driver.get("http://www.google.com")
+
+#specifying the url
+yelptop30 = 'https://www.yelp.com/search?find_desc=Restaurants&find_loc=Austin%2C+TX'
+page = requests.get(yelptop30)
+soup = BeautifulSoup(page.text, 'html.parser')
+names = str(soup.find_all("a", class_="biz-name js-analytics-click"))
+print(names)
+
+newlist = list()
+newlist.append(names)
+for elem in newlist:
+        newlist.extend(elem.strip().split('<span class="indexed-biz-name">'))
+print(newlist)
